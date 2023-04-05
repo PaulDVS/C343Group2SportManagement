e5b2d0488d23b60d08f7ca6aa5f064608f5f3730
aadil <aadilsohail17@gmail.com>
2023-04-05 12:58:11 +0100

    get team by id method implemented

--- pythonProject/classes/teamClass.py
+++ pythonProject/classes/teamClass.py
@@ -16,8 +16,19 @@ def check_team_id(team_id):
     return True
 
 
-def get_team_by_id(match_id):
-    pass
+def get_team_by_id(team_id):
+    config = connectionData.aadil_connection()
+    my_db = pymysql.connect(host=config['host'], user=config['user'], passwd=config['password'],database=config['database'])
+
+    my_cursor = my_db.cursor()
+    sql = f"SELECT * FROM team WHERE teamId = {team_id}"
+    my_cursor.execute(sql)
+
+    # result is just one team as ids are unique hence select the first result([0])
+    result = my_cursor.fetchall()[0]
+
+    # first index is id second is name ..etc
+    return Team(result[0],result[1],result[2],result[3])
 
 class Team:
     def __init__(self, team_id, name, home, country):
@@ -56,4 +67,4 @@ class Team:
     def updateTeam(self):
         pass
 
-
+get_team_by_id(1)
